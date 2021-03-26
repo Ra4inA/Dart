215ba420a0218c7df9d90a34254a2a592f66dd67
Ra4inA <vitya-sorokin-92@mail.ru>
2021-03-26 14:12:22 +0300

    project

---
+++ js/main.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+  $('.feedbacks-slider').slick({
+  	vertical: true,
+  	verticalSwiping: true,
+  	slidesToShow: 2,
+  	slidesToScroll: 2,
+
+
+	  nextArrow: '<a class="next"><i class=" fas fa-chevron-down"></i></a>',
+    prevArrow: '<a class="prev"><i class=" fas fa-chevron-up"></i></a>',
+    
+
+    responsive: [
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: '100px',
+      }
+  }]
+})
+});
+
+
+
+
+
+function Spoiler1() {
+  var ele = document.getElementById("contentSpoiler1");
+  var lel = document.getElementById("")
+  var text = document.getElementById("linkSpoiler1");
+  if(ele.style.display == "inline-block") {
+        ele.style.display = "none";
+        text.style.background = "rgba(250, 250, 250, 1)";
+        ele.style.background = "rgba(250, 250, 250, 1)";
+    }
+  else {
+    	ele.style.display = "inline-block";
+   		text.style.backgroundkground = "rgba(255, 255, 255, 1)";
+  }		ele.style.backgroundkground = "rgba(255, 255, 255, 1)";
+}
+
+
+function Spoiler2() {
+  var ele = document.getElementById("contentSpoiler2");
+  var lel = document.getElementById("")
+  var text = document.getElementById("linkSpoiler2");
+  if(ele.style.display == "inline-block") {
+        ele.style.display = "none";
+        text.style.background = "rgba(250, 250, 250, 1)";
+        ele.style.background = "rgba(250, 250, 250, 1)";
+    }
+  else {
+    	ele.style.display = "inline-block";
+   		text.style.backgroundkground = "rgba(255, 255, 255, 1)";
+  }		ele.style.backgroundkground = "rgba(255, 255, 255, 1)";
+}
+
+
+function Spoiler3() {
+  var ele = document.getElementById("contentSpoiler3");
+  var lel = document.getElementById("")
+  var text = document.getElementById("linkSpoiler3");
+  if(ele.style.display == "inline-block") {
+        ele.style.display = "none";
+        text.style.background = "rgba(250, 250, 250, 1)";
+        ele.style.background = "rgba(250, 250, 250, 1)";
+    }
+  else {
+    	ele.style.display = "inline-block";
+   		text.style.backgroundkground = "rgba(255, 255, 255, 1)";
+  }		ele.style.backgroundkground = "rgba(255, 255, 255, 1)";
+}
+
+
+function Spoiler4() {
+  var ele = document.getElementById("contentSpoiler4");
+  var lel = document.getElementById("")
+  var text = document.getElementById("linkSpoiler4");
+  if(ele.style.display == "inline-block") {
+        ele.style.display = "none";
+        text.style.background = "rgba(250, 250, 250, 1)";
+        ele.style.background = "rgba(250, 250, 250, 1)";
+    }
+  else {
+    	ele.style.display = "inline-block";
+   		text.style.backgroundkground = "rgba(255, 255, 255, 1)";
+  }		ele.style.backgroundkground = "rgba(255, 255, 255, 1)";
+}
+
+
+function Spoiler5() {
+  var ele = document.getElementById("contentSpoiler5");
+  var text = document.getElementById("linkSpoiler5");
+  if(ele.style.display == "inline-block") {
+        ele.style.display = "none";
+        text.style.background = "rgba(250, 250, 250, 1)";
+        ele.style.background = "rgba(250, 250, 250, 1)";
+    }
+  else {
+    	ele.style.display = "inline-block";
+   		text.style.backgroundkground = "rgba(255, 255, 255, 1)";
+  }		ele.style.backgroundkground = "rgba(255, 255, 255, 1)";
+}
